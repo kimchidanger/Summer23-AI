f9dabff37b6e7597145a8d95db53a922f31e1c3d
Elijah <elijah.j.choi@gmail.com>
2023-06-28 12:50:43 -0400

    PythonClasses.py

---
+++ PythonClasses.py
@@ -0,0 +1,10 @@
+class Student:
+    def _init_(self, name, course):
+        self.name = name
+        self.course = course
+
+    def myfunc(self):
+        print("Hi, my name is " + self.name ". I am taking " + self.course)
+    
+    p1 = Student("John", "math")
+    p1.myfunc()
