d730e24021c84c80e914306e8756c1fc2edccbb2
kimchidanger <137923048+kimchidanger@users.noreply.github.com>
2023-06-27 22:44:09 -0400

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-choice = input("hours to minutes? [y]/[n]")
-if (choice == "y"):
-    choice2 = int (input("how mnay hours?"))
-    print(choice2*60)
-elif (choice == "n"):
-    choice3 = int (input("how mnay minutes?"))
-    print(choice3/60)
