4db3bab1e3361964f0921cc86d1594182607952b
Elijah <elijah.j.choi@gmail.com>
2023-06-29 12:47:12 -0400

    Importing Libraries Practice 2

--- ImportingLibraries2.py
+++ ImportingLibraries2.py
@@ -8,7 +8,7 @@ result_1 = pow(2, 4)
 print("result_1 is", result_1)
 
 result_2 = sqrt(16)
-print("result_2 is", result_2)
+print("result_2 is", result_2) 
 
 result_3 = randint(0,100)
 print("result_3 is", result_3)
