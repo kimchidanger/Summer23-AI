71235eeeb2ead90e09d1f87a3bdbdb11afc4e69e
Elijah <elijah.j.choi@gmail.com>
2023-06-29 12:47:45 -0400

    Importing Libraries Practice 3

--- ImportingLibraries3.py
+++ ImportingLibraries3.py
@@ -7,7 +7,7 @@ print("result_1 is", result_1)
 result_2 = red.sqrt(16)
 print("result_2 is", result_2)
 
-result_3 = blue.randint(0,100)
+result_3 = blue.randint(0,100) 
 print("result_3 is", result_3)
 
 names = ["Bob", "Jeff", "Bill", "Elena", "Tim", "Dominic", "Joe", "Eric"]
