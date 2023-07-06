d637fbbeeed69254b86a02559457099e28d029c7
Elijah <elijah.j.choi@gmail.com>
2023-07-06 12:32:18 -0400

    KB Practice

--- kbase.pl
+++ kbase.pl
@@ -1,13 +1,13 @@
 servesAll(american, [salad, steak, sandwiches, burgers, fried_chicken]).
 servesAll(burger_place, [burgers, fries, onion_rings]).
-servesAll(chinese, [eggrolls, rice, shrimp, soup, and noodles]).
-servesAll(indian, [papadam, bagan_bharta, rice, tandoori, and naan]).
-servesAll(italian, [salad, pasta, cioppino, snapper, bread, and garlic_bread]).
-servesAll(japanese, [sashimi, rice, tempura, and noodles]).
-servesAll(mediterranean, [gyros, hummus, pita, and falafel]).
-servesAll(mexican, [tacos, beans, rice, enchiladas, and fish_tacos]).
+servesAll(chinese, [eggrolls, rice, shrimp, soup, noodles]).
+servesAll(indian, [papadam, bagan_bharta, rice, tandoori, naan]).
+servesAll(italian, [salad, pasta, cioppino, snapper, bread, garlic_bread]).
+servesAll(japanese, [sashimi, rice, tempura, noodles]).
+servesAll(mediterranean, [gyros, hummus, pita, falafel]).
+servesAll(mexican, [tacos, beans, rice, enchiladas, fish_tacos]).
 servesAll(pizza_place, [pizza, salad, garlic_bread]).
-servesAll(thai, [rice, noodles, larb, and pad_thai]).
+servesAll(thai, [rice, noodles, larb, pad_thai]).
 
 serves(Kind, Dish) :- 
     servesAll(Kind, Dishes), 
